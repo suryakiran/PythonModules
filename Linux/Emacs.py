a3444508c0cbb9db99d6405500fa0abcaf85fd94
Surya Kiran Gullapalli <SuryaKiran.Gullapalli@gmail.com>
2012-12-31 00:07:25 +0530

    Get server names from ~/.emacs.d/server directory

--- Linux/Emacs.py
+++ Linux/Emacs.py
@@ -1,15 +1,19 @@
 import os, sys
 from Locator import Locator
 from Launcher import Launcher
+import commands
 import subprocess
 
 class EmacsPrivate:
     def __init__(self):
         self.exePath = Locator('emacs')()
-        self.emacsClientPath = Locator('emacsclient')()
-        homeEnv = os.environ['HOME']
+        self.exeClientPath = Locator('emacsclient')()
+        self.homeEnv = os.environ['HOME']
         self._launcher = Launcher(self.exeClientPath)
 
     def serverList(self):
-        return subprocess.check_output([self.exePath, '--serverlist']).split()
+        servers_dir = os.path.join(self.homeEnv, '.emacs.d', 'server')
+        files = commands.getoutput("ls -l %s | grep '^-' | awk '{print $NF}'" % servers_dir).split('\n')
         
+    def launcher(self):
+        return self._launcher

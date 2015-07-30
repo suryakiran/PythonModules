c71e228fcc0c3d495ebb95095bb41680b6791395
Surya Gullapalli <surya.gullapalli@ddu-india.com>
2015-07-30 09:43:53 +0530

    Run in sub directory

--- Launcher.py
+++ Launcher.py
@@ -86,10 +86,12 @@ class Launcher:
             return
         
         if wait:
+            if run_in_dir:
+                os.chdir(run_in_dir)
+            print os.getcwd()
             os.execl(self.args[0], *self.args)
         else:
             if run_in_dir:
-                print run_in_dir
                 subprocess.Popen(self.args, stdout = stdout, stderr = stderr, cwd=run_in_dir)
             else:
                 subprocess.Popen(self.args, stdout = stdout, stderr = stderr)
